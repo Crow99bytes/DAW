229039bfa64f28672bed64092f550b0cfed5cf55
manueld99 <manueld200sx@gmail.com>
2026-01-19 20:45:22 +0100

    Add Person class and related subclasses for POO exercises
    Introduced the Person class with properties and methods in cuarto.php. Added Salesman, ComputerProgrammer, and WebDeveloper subclasses in quinto.php, each extending Person and overriding or extending its methods for specific behaviors.

---
+++ DWCS/Segunda Evaluación/Unidad 4 - POO y Symfony/a4.1/cuarto.php
@@ -0,0 +1,25 @@
+<?php
+    class Person {
+        const species = "Homo Sapiens";
+        
+        public string $name = "";
+        public int $age = 0;
+        public string $occupation = "";
+        
+        public function __construct($name, $age, $occupation) {
+            $this->name = $name;
+            $this->age = $age;
+            $this->occupation = $occupation;
+        }
+        
+        public function introduce() {
+            return "Hello, my name is {$this->name}";
+        }
+        public function describe_job() {
+            return "I am currently working as a(n) {$this->occupation}";
+        }
+        public static function greet_extraterrestrials($species) {
+            return "Welcome to Planet Earth {$species}!";
+        }
+    }
+?>
